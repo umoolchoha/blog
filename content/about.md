3e801df8e8a8c334536ccb28f3a6044377e65148
umoolchoha <umoolchoha@gmail.com>
2019-06-09 23:27:52 +0900

    changed

---
+++ content/about.md
@@ -0,0 +1,11 @@
+---
+title: 'hello'
+date: 2019-03-18T21:34:36+09:00
+categories:
+    - about
+tags : 
+    - umool
+type: posts
+---
+
+hello
---
+++ deploy.bat
@@ -0,0 +1,12 @@
+hugo -t hyde-hyde
+
+cd public
+git add .
+git commit -m "changed"
+git push
+
+cd ..
+git add .
+git commit -m "changed"
+git push
+
